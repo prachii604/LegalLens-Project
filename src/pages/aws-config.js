eb906523960009be901cec92c18b3dab2aa768b3
unknown <prachita.srivastava604@gmail.com>
2025-10-05 23:21:31 +0530

    added region in aws-config.js

--- src/pages/aws-config.js
+++ src/pages/aws-config.js
@@ -1,6 +1,6 @@
 export const poolData = {
   UserPoolId: "ap-south-1_JXx72K8QQ",          // <-- your User Pool ID
   ClientId:   "3rilj8kumat6ilpobf2q9ja4ga",     // <-- your App Client ID (no secret)
-  region: ap-south-1
+  region: "ap-south-1"
 
 };
